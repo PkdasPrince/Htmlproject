367949068f8c3c73e98acd35ebc8a18e3b35eca9
Pkdas Prince <82633423+PkdasPrince@users.noreply.github.com>
2021-06-30 14:10:38 +0600

    Add files via upload

---
+++ Htmlproject/js/main.js
@@ -0,0 +1,9 @@
+<script>
+  $(document).ready(function(){
+    $('.slider').bxSlider();
+    $('#main-menu').meanmenu( {
+      meanMenuContainer:"#mobile-menu",
+      meanScreenWidth:768,
+    });
+  });
+</script>
